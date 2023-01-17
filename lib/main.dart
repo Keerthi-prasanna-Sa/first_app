bf878f55866c34452faf6ad8eeb9d1c3a832cff1
Keerthi-prasanna-Sa <keerthiprasanna01@gmail.com>
2023-01-17 11:06:22 +0530

    with the layout Assignment

--- lib/main.dart
+++ lib/main.dart
@@ -13,83 +13,40 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         home: Scaffold(
           body: SafeArea(
-              // child: Text("Hello World",
-              //        style: TextStyle(color: Colors.blue[400],
-              //        fontWeight: FontWeight.w600,
-              //        fontSize: 60,
-              //        fontFamily: "Fruktur",
-              //        fontStyle: FontStyle.italic),),
-              child: Center(
-                // child: Container(
-                //   padding: const EdgeInsets.all(20),
-                //   // color: Colors.redAccent,
-                //   width: 250,
-                //   height: 250,
-                //   decoration: BoxDecoration(
-                //     gradient: const LinearGradient(
-                //       colors: [Colors.orange, Colors.white, Colors.green], 
-                //       begin: Alignment.topLeft, 
-                //       end: Alignment.bottomRight),
-                //     border: Border.all(color: Colors.black, width: 5),
-                //     borderRadius: const BorderRadius.only(
-                //       topLeft: Radius.circular(60), 
-                //       topRight: Radius.circular(10), 
-                //       bottomLeft: Radius.circular(10), 
-                //       bottomRight: Radius.circular(60)
-                //       ),
-                //     boxShadow: const [
-                //       BoxShadow(
-                //         color: Colors.black45, 
-                //         offset: Offset(0,0), 
-                //         blurRadius: 20, 
-                //         spreadRadius: 25),
-                //       BoxShadow(
-                //         color: Colors.greenAccent, 
-                //         offset: Offset(0,0), 
-                //         blurRadius: 20, 
-                //         spreadRadius: 5)
-                //       ]
-                //     ),
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [ Container(
-                      width: 300,
-                      height: 300,
-                      padding: EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 70, 205, 140),
-                      borderRadius: BorderRadius.circular(30)),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(30),
-                        child: Image.asset("images/image1.jpg",
-                        fit: BoxFit.cover
-                        ),
-                      ),
-                    ),
-                    Container(
-                alignment: Alignment.center,
-                child: const Text(
-                  'COFFEE',
-                  style: TextStyle(
-                    fontFamily: "GravitasOne",
-                    color: Colors.white,  
-                    fontWeight: FontWeight.w200,
-                    fontSize: 45.0),
-                  ),
-               ),
-             ],
-           ),
-         )
+              child: Row(
+                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // child 1
+                Container(
+                  padding: EdgeInsets.only(top: 10),
+                  alignment: Alignment.topCenter,
+                  width: 90,
+                  height: double.infinity,
+                  color: Colors.red,
+                  child: Text("I am red"),
+                ),
+                // child 2
+                Container(
+                  padding: EdgeInsets.only(top: 10),
+                  alignment: Alignment.topCenter,
+                  width: 90,
+                  height: double.infinity,
+                  color: Colors.blue,
+                  child: Text("I am blue"),
+                ),
+                // child 3
+                Container(
+                  padding: EdgeInsets.only(top: 10),
+                  alignment: Alignment.topCenter,
+                  width: 90,
+                  height: double.infinity,
+                  color: Colors.green,
+                  child: Text("I am green"),
+                  )
+            ])
        )
      )
    );
   }
 }
 
-// Images
-
-// Assets Images
-
-// File Images
-
-// Network Images
